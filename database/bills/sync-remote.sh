4dee8c528e5461c09c25f8867e80d4cec23b976a
Luighi <luighiavz@gmail.com>
2022-02-09 22:52:49 -0500

    Update scripts to upload to Google Drive
    Check if backup file exists to upload, otherwise, regenerate it with
    create-image.sh. This validation is useful when the image is present, to
    prevent the regeneration in GitHub actions.

--- database/bills/sync-remote.sh
+++ database/bills/sync-remote.sh
@@ -3,8 +3,20 @@
 #Init directory 
 INIT_DIR=${PWD}
 
-#1. Create image
-./projects/create-image.sh
+#Expected backup
+if [[ -z $BACKUP_FOLDER ]]; then
+  BACKUP_FOLDER=${INIT_DIR}/backup
+fi
+
+BACKUP_NAME=backup_bills.sql
+BACKUP_FILE=$BACKUP_FOLDER/$BACKUP_NAME
+
+#1. Create image if not exists
+if [[ ! -f $BACKUP_FILE ]]; then
+  echo "Backup file not found, creating it..."
+  ./projects/create-image.sh
+  echo "Backup created"
+fi
 
 #2. Update remote backup
 DRIVE_SYNC_FOLDER=drive-sync 
@@ -13,13 +25,8 @@ if [[ ! -z $GOOGLE_AUTH_ENCODED ]]; then
 	echo $GOOGLE_AUTH_ENCODED | ${INIT_DIR}/tools/decode-key.sh > $DRIVE_SYNC_FOLDER/key.json
 fi
 
-if [[ -z $BACKUP_FOLDER ]]; then
-  BACKUP_FOLDER=${INIT_DIR}/backup
-fi
-
-BACKUP_NAME=backup_projects.sql
 
 cd $DRIVE_SYNC_FOLDER
 npm ci --only=production
-npm run update  -- --type=projects --source-path=$BACKUP_FOLDER/$BACKUP_NAME
+npm run update  -- --type=projects --source-path=$BACKUP_FILE
 
