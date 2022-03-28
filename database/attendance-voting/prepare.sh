ce4db8e648f79e8f253374756a57f827a3042c27
Luighi <luighiavz@gmail.com>
2022-03-28 00:49:03 -0500

    Add voting and attendance

---
+++ database/attendance-voting/prepare.sh
@@ -0,0 +1,59 @@
+#!/bin/bash 
+#Init directory 
+INIT_DIR=${PWD}
+
+if [[ -z $PROJECT_DIRECTORY ]]; then
+	PROJECT_DIRECTORY=${HOME}/congreso
+fi
+
+# Verify environment
+if [[ $ENV_TYPE == "production" ]]; then
+	ENV_DIRECTORY=${PROJECT_DIRECTORY}/production
+else
+	ENV_DIRECTORY=${PROJECT_DIRECTORY}/staging
+fi
+
+GIT_BRANCH="main"
+
+mkdir -p ${ENV_DIRECTORY}
+
+DATA_DIRECTORY=${ENV_DIRECTORY}/congreso-pleno-asistencia-votacion
+
+# Check if directory and repository exists
+if [[ -d $DATA_DIRECTORY ]]; then
+	cd ${DATA_DIRECTORY}
+	#Check if is a repository
+	if git rev-parse --is-inside-work-tree > /dev/null 2>&1 ; then
+		git fetch origin ${GIT_BRANCH}
+		git reset --hard origin/${GIT_BRANCH}
+	else
+		cd ../
+		rm -rf ${DATA_DIRECTORY}
+	  git clone https://github.com/openpolitica/congreso-pleno-asistencia-votacion.git
+		cd ${DATA_DIRECTORY}
+		git checkout origin/${GIT_BRANCH}
+	fi
+else 
+	cd ${ENV_DIRECTORY}
+	git clone https://github.com/openpolitica/congreso-pleno-asistencia-votacion.git
+	cd ${DATA_DIRECTORY}
+	git checkout origin/${GIT_BRANCH}
+fi
+
+
+cd ${DATA_DIRECTORY}
+#Build databases
+rm -rf *.db*
+mkdir -p ~/.m2
+docker run -it --rm --name attendance-voting \
+	-v ~/.m2:/var/maven/.m2 -u 1000 -e MAVEN_CONFIG=/var/maven/.m2 \
+	-v "$(pwd)":/usr/src/mymaven \
+	-w /usr/src/mymaven maven:3.8.4-openjdk-17 \
+	/bin/bash -c "mvn clean compile exec:java -Duser.home=/var/maven -Dexec.mainClass='op.congreso.pleno.CargaPlenos'; mvn exec:java -Duser.home=/var/maven -Dexec.mainClass='op.congreso.pleno.CargaRegitroPlenos'"
+
+docker run -it --rm --name plenary-days \
+	-v "$(pwd)":/usr/src/myapp \
+	-w /usr/src/myapp python:3.9 \
+	/bin/bash -c "pip install csvs-to-sqlite; csvs-to-sqlite plenos.csv plenos.db; chown 1000:1000 plenos.db"
+
+
