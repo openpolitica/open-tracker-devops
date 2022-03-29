304907a78c82db03dada37166cafc6fc45ec70a2
Luighi <luighiavz@gmail.com>
2022-03-29 10:26:47 -0500

    Delete interactive options to docker to execute in github action

--- database/attendance-voting/prepare.sh
+++ database/attendance-voting/prepare.sh
@@ -38,13 +38,13 @@ cd ${DATA_DIRECTORY}
 #Build databases
 rm -rf *.db*
 mkdir -p ~/.m2
-docker run -it --rm --name attendance-voting \
+docker run --rm --name attendance-voting \
 	-v ~/.m2:/var/maven/.m2 -u 1000 -e MAVEN_CONFIG=/var/maven/.m2 \
 	-v "$(pwd)":/usr/src/mymaven \
 	-w /usr/src/mymaven maven:3.8.4-openjdk-17 \
 	/bin/bash -c "mvn clean compile exec:java -Duser.home=/var/maven -Dexec.mainClass='op.congreso.pleno.CargaPlenos'; mvn exec:java -Duser.home=/var/maven -Dexec.mainClass='op.congreso.pleno.CargaRegitroPlenos'"
 
-docker run -it --rm --name plenary-days \
+docker run --rm --name plenary-days \
 	-v "$(pwd)":/usr/src/myapp \
 	-w /usr/src/myapp python:3.9 \
 	/bin/bash -c "pip install csvs-to-sqlite; csvs-to-sqlite plenos.csv plenos.db; chown 1000:1000 plenos.db"
